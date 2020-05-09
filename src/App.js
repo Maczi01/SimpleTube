553c20c7743b5d9186a4c73abf25479ef5140f01
Maczi01 <mateusz.w.twardy@gmail.com>
2020-05-09 18:54:48 +0200

    move data to state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import React from 'react';
 import Header from "./components/Header";
 import VideoList from "./components/VideoList";
 import VideoPlayer from "./components/VideoPlayer";
-import db from './db'
 
 class App extends React.Component {
 
     state = {
-        movieList: []
+        movieList: [
+            {
+                "title": "Flume - Hyperreal",
+                "description": "Flume - Hyperreal feat. Kučka\nNew Flume apparel out now: http://flu.me/Store",
+                "video_url": "https://www.youtube.com/watch?v=SPc-oWXEhQQ"
+            },
+            {
+                "title": "BMX FLATLAND ",
+                "description": "BMX FLATLAND FINALS HIGHLIGHTS - FISE CHINA 2019",
+                "video_url": "https://www.youtube.com/watch?v=utPcddvD19o"
+            },
+            {
+                "title": "Mega hits 2020",
+                "description": "Dua Lipa, Coldplay, Martin Garrix & Kygo, The Chainsmokers Style - Feeling Me ",
+                "video_url": "https://www.youtube.com/watch?v=OFc28hTZQL0"
+            }
+        ]
     }
 
     componentDidMount() {
-        // fetch('http://localhost:3000/videos')
-        fetch(db)
+        // this.getMovieList();
+    }
+
+    getMovieList() {
+        fetch('http://localhost:3000/videos')
             .then((response) => (response.json()))
             .then(booksList => {
                 this.setState({movieList: booksList});
