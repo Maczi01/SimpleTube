5072bb66cf5fd1b0a685abeaaf791892dee2d7a4
Maczi01 <mateusz.w.twardy@gmail.com>
2020-05-09 17:35:48 +0200

    add data to state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ import VideoPlayer from "./components/VideoPlayer";
 class App extends React.Component {
 
     state = {
-        movieList: []
+        movieList: [
+            {
+                "title": "Flume - Hyperreal",
+                "description": "Flume - Hyperreal feat. Kučka\nNew Flume apparel out now: http://flu.me/Store",
+                "video_url": "https://www.youtube.com/watch?v=SPc-oWXEhQQ"
+            },
+            {
+                "title": "BMX FLATLAND ",
+                "description": "BMX FLATLAND FINALS HIGHLIGHTS - FISE CHINA 2019",
+                "video_url": "https://www.youtube.com/watch?v=utPcddvD19o"
+            },
+            {
+                "title": "Mega hits 2020",
+                "description": "Dua Lipa, Coldplay, Martin Garrix & Kygo, The Chainsmokers Style - Feeling Me ",
+                "video_url": "https://www.youtube.com/watch?v=OFc28hTZQL0"
+            }
+        ]
     }
 
     componentDidMount() {
